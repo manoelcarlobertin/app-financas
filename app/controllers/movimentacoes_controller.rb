1de06f8af40556486cebd80e6a47eb7a584d6a52
Manoel Carlo Bertin <manoelcarlobertin@outlook.com>
2024-09-05 09:38:22 -0300

    Em config/routes.rb por motivo de seguranca em resources disponibilizei as actions que estão no meu movimentacoes_controller e também inclui um root.

--- app/controllers/movimentacoes_controller.rb
+++ app/controllers/movimentacoes_controller.rb
@@ -1,23 +1,15 @@
 class MovimentacoesController < ApplicationController
-  before_action :set_movimentacao, only: %i[ show edit update destroy ]
+  before_action :set_movimentacao, only: %i[ destroy ]
 
   # GET /movimentacoes or /movimentacoes.json
   def index
-    @movimentacoes = Movimentacao.order(data: :desc)
+    @movimentacoes = Movimentacao.order(data: :desc, created_at: :desc)
     @saldo = Movimentacao.saldo_atual
-  end
-
-  # GET /movimentacoes/1 or /movimentacoes/1.json
-  def show
   end
 
   # GET /movimentacoes/new
   def new
     @movimentacao = Movimentacao.new
-  end
-
-  # GET /movimentacoes/1/edit
-  def edit
   end
 
   # POST /movimentacoes or /movimentacoes.json
@@ -26,35 +18,19 @@ class MovimentacoesController < ApplicationController
 
     respond_to do |format|
       if @movimentacao.save
-        format.html { redirect_to movimentacao_url(@movimentacao), notice: "Movimentacao was successfully created." }
-        format.json { render :show, status: :created, location: @movimentacao }
+        format.html { redirect_to movimentacoes_url, notice: "Movimentacao was successfully created." }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @movimentacao.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /movimentacoes/1 or /movimentacoes/1.json
-  def update
-    respond_to do |format|
-      if @movimentacao.update(movimentacao_params)
-        format.html { redirect_to movimentacao_url(@movimentacao), notice: "Movimentacao was successfully updated." }
-        format.json { render :show, status: :ok, location: @movimentacao }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @movimentacao.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /movimentacoes/1 or /movimentacoes/1.json
   def destroy
-    @movimentacao.destroy!
+    @movimentacao.destroy
 
     respond_to do |format|
       format.html { redirect_to movimentacoes_url, notice: "Movimentacao was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
