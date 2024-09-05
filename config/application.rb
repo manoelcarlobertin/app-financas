65197d66886579a5cf44406312f3b87e4a596664
Manoel Carlo Bertin <manoelcarlobertin@outlook.com>
2024-09-05 08:25:53 -0300

    Em app/config/application.rb acrescentei a configuração o i18n para fazer a conversão de dolar para reais e mostrar na minha view index.

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,9 @@ module AppFinancas
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
 
+    # Internacionalização
+    config.i18n.default_locale = :"pt-BR"
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
