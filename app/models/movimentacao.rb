9186871fac2c8acf8c22ac2d2ab3c3d5cea87070
Manoel Carlo Bertin <manoelcarlobertin@outlook.com>
2024-09-05 00:40:27 -0300

    my first commit

---
+++ app/models/movimentacao.rb
@@ -0,0 +1,25 @@
+class Movimentacao < ApplicationRecord
+  enum :tipo, { saida: "saida", entrada: "entrada" }
+
+  # validates :data, presence: true
+  validates :data, comparison: { less_than_or_equal_to: proc { Date.current } }
+  validates :descricao, presence: true
+  validates :valor, presence: true
+  validates :tipo, presence: true
+
+  validate :valida_se_existe_saldo
+
+  def self.saldo_atual
+    self.entrada.sum(:valor) - self.saida.sum(:valor)
+  end
+
+  private
+
+  def valida_se_existe_saldo
+    return if entrada?
+    return if valor.to_f <= Movimentacao.saldo_atual
+    # if valor.present? && valor > Movimentacao.saldo_atual # poderia "valor.to_f" cast pra float.
+    # pra validar tem que ser uma entrada e valor menor que o saldo atual, caso contrário ERROR.
+    errors.add :valor, "NÃO HÁ SALDO SUFICIENTE."
+  end
+end
