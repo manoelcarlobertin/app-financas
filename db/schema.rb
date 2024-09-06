8f3fd577b656b984dc54acfeb4b7c0f167c3df02
Manoel Carlo Bertin <manoelcarlobertin@outlook.com>
2024-09-06 09:56:44 -0300

    Agora deu certo ao rodar a migrate as tabelas foram modificadas corretamente.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_05_135730) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_06_124748) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_05_135730) do
     t.string "tipo", limit: 50
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_movimentacoes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +36,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_05_135730) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "movimentacoes", "users"
 end
