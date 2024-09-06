1c3a6be705ecf4807fd2bd34d651e5efdc48cc72
Manoel Carlo Bertin <manoelcarlobertin@outlook.com>
2024-09-06 09:48:39 -0300

    Refiz isso adiante -> Adicionei um campo user na tabela movimentacoes pra identificar de quem é as acoes, e para não dar erro, o campo user coloquei na migration null=true.

--- db/migrate/20240906124748_add_user_to_movimentacoes.rb
+++ db/migrate/20240906124748_add_user_to_movimentacoes.rb
@@ -1,5 +1,5 @@
 class AddUserToMovimentacoes < ActiveRecord::Migration[7.2]
   def change
-    add_reference :movimentacoes, :user, null: true, foreing_key: true
+    add_reference :movimentacoes, :user, null: false, foreign_key: true
   end
 end
